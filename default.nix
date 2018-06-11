b0118d0905e827ca0346a632121bf4a52d48ae8c
Tobias Happ <tobias.happ@gmx.de>
2018-06-11 02:03:28 +0200

    Adds xsetroot and alsaUtils as runtimeDeps

--- default.nix
+++ default.nix
@@ -1,5 +1,9 @@
 with import <nixpkgs> { };
 
+let
+  runtimeDeps = [ alsaUtils xorg.xsetroot ];
+in
+
 rustPlatform.buildRustPackage rec {
   name = "dwm-status";
 
@@ -9,8 +13,14 @@ rustPlatform.buildRustPackage rec {
     dbus
     gdk_pixbuf
     libnotify
+    makeWrapper
     pkgconfig
   ];
 
   cargoSha256 = "0169k91pb7ipvi0m71cmkppp1klgp5ghampa7x0fxkyrvrf0dvqg";
+
+  postInstall = ''
+    wrapProgram $out/bin/${name} \
+      --prefix "PATH" : "${stdenv.lib.makeBinPath runtimeDeps}"
+  '';
 }
