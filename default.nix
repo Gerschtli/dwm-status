2b5a599cd2f0ba11a380636ffadd080ef8ec4fe3
Tobias Happ <tobias.happ@gmx.de>
2018-06-20 02:14:32 +0200

    Adds config crate

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ rustPlatform.buildRustPackage rec {
   nativeBuildInputs = [ makeWrapper pkgconfig ];
   buildInputs = [ dbus gdk_pixbuf libnotify xorg.libX11 ];
 
-  cargoSha256 = "0qr999hwrqn7a4n4kvbrpli7shxp9jchj8csxzsw951qmzq32qwv";
+  cargoSha256 = "0aq9rnfy76cqzc48n2br3fj1llapxib95zd92q9gfyxvwp5ay58a";
 
   postInstall = ''
     wrapProgram $out/bin/${name} \
