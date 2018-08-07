e894d4104d43032f20bc001897ccc8fb75d3cbfc
Tobias Happ <tobias.happ@gmx.de>
2018-08-07 13:08:54 +0200

    Update time feature with second precision after hibernate

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,8 +5,11 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-### Changes
+### Changed
 - Clippy propagates warnings to errors
+
+### fixed
+- Update time feature with second precision after hibernate
 
 ## [1.1.1] - 2018-06-28
 ### Added
--- src/features/time/feature.rs
+++ src/features/time/feature.rs
@@ -2,10 +2,13 @@ use super::TimeData;
 use super::FEATURE_NAME;
 use async;
 use chrono;
+use chrono::Timelike;
 use error::*;
 use feature;
 use settings;
 use std::sync::mpsc;
+use std::thread;
+use std::time;
 
 #[derive(Debug)]
 pub struct Time {
@@ -37,24 +40,21 @@ impl feature::Feature for Time {
     feature_default!();
 
     fn init_notifier(&self) -> Result<()> {
-        let (interval, delay) = if self.settings.update_seconds {
-            (1, None)
-        } else {
-            let seconds_string = format!("{}", chrono::Local::now().format("%S"));
-            let seconds = seconds_string
-                .parse::<u64>()
-                .wrap_error(FEATURE_NAME, "could extract initial delay seconds")?;
+        let id = self.id.clone();
+        let tx = self.tx.clone();
+        let update_seconds = self.settings.update_seconds;
 
-            (60, Some(60 - seconds))
-        };
+        thread::spawn(move || loop {
+            async::send_message(FEATURE_NAME, &id, &tx);
 
-        async::send_message_interval(
-            FEATURE_NAME,
-            self.id.clone(),
-            self.tx.clone(),
-            interval,
-            delay,
-        );
+            let update_interval = if update_seconds {
+                1
+            } else {
+                60 - chrono::Local::now().second()
+            };
+
+            thread::sleep(time::Duration::from_secs(update_interval as u64));
+        });
 
         Ok(())
     }
