b3c5d156927514d3e917b5667d2f69328ac14a26
Tobias Happ <tobias.happ@gmx.de>
2018-11-25 17:15:14 +0100

    Refactor StatusBar to encapsulate logic of lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -36,9 +36,7 @@ mod utils;
 
 use error::*;
 use status_bar::StatusBar;
-use std::collections::HashMap;
 use std::env;
-use std::ops::DerefMut;
 use std::sync::mpsc;
 
 fn get_settings() -> Result<settings::Settings> {
@@ -51,65 +49,6 @@ fn get_settings() -> Result<settings::Settings> {
     settings::Settings::new(&path).wrap_error("settings", "error creating settings object")
 }
 
-fn render(
-    tx: &mpsc::Sender<async::Message>,
-    rx: &mpsc::Receiver<async::Message>,
-    order: &[uuid::Uuid],
-    feature_map: &mut HashMap<uuid::Uuid, Box<feature::Feature>>,
-    settings: &settings::Settings,
-) -> Result<()> {
-    let tx = tx.clone();
-    ctrlc::set_handler(move || {
-        tx.send(async::Message::Kill)
-            .wrap_error_kill("termination", "notify thread killed");
-    })
-    .wrap_error("termination", "failed to set termination handler")?;
-
-    let status_bar = StatusBar::new(settings.separator.clone())?;
-    status_bar.render(order, feature_map)?;
-
-    for message in rx {
-        match message {
-            async::Message::FeatureUpdate(ref id) => {
-                match feature_map.get_mut(id) {
-                    Some(ref mut feature) => {
-                        update_feature(feature.deref_mut().deref_mut(), settings.debug)?;
-                    },
-                    None => {
-                        return Err(Error::new_custom(
-                            "invalid message",
-                            &format!("message id {} does not exist", id),
-                        ))
-                    },
-                };
-
-                status_bar.render(order, feature_map)?;
-            },
-            async::Message::Kill => break,
-            async::Message::UpdateAll => {
-                if settings.debug {
-                    println!("update all");
-                }
-                for feature in feature_map.values_mut() {
-                    update_feature(feature.deref_mut(), settings.debug)?;
-                }
-            },
-        }
-    }
-
-    Ok(())
-}
-
-fn update_feature(feature: &mut feature::Feature, debug: bool) -> Result<()> {
-    feature.update()?;
-
-    if debug {
-        println!("update {}: {}", feature.name(), feature.render());
-    }
-
-    Ok(())
-}
-
 pub fn run() -> Result<()> {
     let (tx, rx) = mpsc::channel();
 
@@ -119,7 +58,6 @@ pub fn run() -> Result<()> {
     for feature_name in &settings.order {
         let mut feature = features::create_feature(&feature_name, &tx, &settings)?;
         feature.init_notifier()?;
-        feature.update()?;
         features.push(feature);
     }
 
@@ -127,14 +65,25 @@ pub fn run() -> Result<()> {
         return Err(Error::new_custom("settings", "no features enabled"));
     }
 
-    let order: Vec<_> = features.iter().map(|x| x.id()).collect();
-
-    let mut feature_map: HashMap<_, _> = features
-        .into_iter()
-        .map(|feature| (feature.id(), feature))
-        .collect();
-
     resume::init_resume_notifier(&tx)?;
 
-    render(&tx, &rx, &order, &mut feature_map, &settings)
+    tx.send(async::Message::UpdateAll)
+        .wrap_error("init", "initial update message failed")?;
+
+    ctrlc::set_handler(move || {
+        tx.send(async::Message::Kill)
+            .wrap_error_kill("termination", "notify thread killed");
+    })
+    .wrap_error("termination", "failed to set termination handler")?;
+
+    let mut status_bar = StatusBar::new(features)?;
+
+    for message in rx {
+        match message {
+            async::Message::Kill => break,
+            _ => status_bar.update(&message, &settings)?,
+        }
+    }
+
+    Ok(())
 }
--- src/status_bar.rs
+++ src/status_bar.rs
@@ -1,7 +1,9 @@
 #![allow(unsafe_code)]
 
+use async;
 use error::*;
 use feature;
+use settings;
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::os::raw::c_char;
@@ -10,13 +12,97 @@ use uuid;
 use x11::xlib;
 
 pub struct StatusBar {
-    display: *mut xlib::Display,
-    root_window: xlib::Window,
-    separator: String,
+    feature_map: HashMap<uuid::Uuid, Box<dyn feature::Feature>>,
+    order: Vec<uuid::Uuid>,
+    string_map: HashMap<uuid::Uuid, String>,
+    xsetroot: XSetRoot,
 }
 
 impl StatusBar {
-    pub fn new(separator: String) -> Result<Self> {
+    pub fn new(features: Vec<Box<dyn feature::Feature>>) -> Result<Self> {
+        let order: Vec<_> = features.iter().map(|feature| feature.id()).collect();
+
+        let string_map: HashMap<_, _> = features
+            .iter()
+            .map(|feature| (feature.id(), String::new()))
+            .collect();
+
+        let feature_map: HashMap<_, _> = features
+            .into_iter()
+            .map(|feature| (feature.id(), feature))
+            .collect();
+
+        Ok(StatusBar {
+            feature_map,
+            order,
+            string_map,
+            xsetroot: XSetRoot::new()?,
+        })
+    }
+
+    pub fn update(
+        &mut self,
+        message: &async::Message,
+        settings: &settings::Settings,
+    ) -> Result<()> {
+        match message {
+            async::Message::FeatureUpdate(id) if self.feature_map.contains_key(id) => {
+                self.update_feature(*id, &settings)?;
+                self.render(&settings)?;
+            },
+            async::Message::FeatureUpdate(id) => {
+                return Err(Error::new_custom(
+                    "invalid message",
+                    &format!("message id {} does not exist", id),
+                ));
+            },
+            async::Message::UpdateAll => {
+                if settings.debug {
+                    println!("update all");
+                }
+
+                for id in self.order.clone() {
+                    self.update_feature(id, &settings)?;
+                }
+                self.render(&settings)?;
+            },
+            _ => (),
+        }
+
+        Ok(())
+    }
+
+    fn update_feature(&mut self, id: uuid::Uuid, settings: &settings::Settings) -> Result<()> {
+        let feature = self.feature_map.get_mut(&id).unwrap();
+        let rendered = feature.update()?.render(&settings);
+
+        if settings.debug {
+            println!("update {}: {}", feature.name(), &rendered);
+        }
+
+        self.string_map.insert(id, rendered);
+        Ok(())
+    }
+
+    pub fn render(&self, settings: &settings::Settings) -> Result<()> {
+        let status = self
+            .order
+            .iter()
+            .map(|id| &self.string_map[id][..])
+            .collect::<Vec<_>>()
+            .join(&settings.separator);
+
+        self.xsetroot.render(status)
+    }
+}
+
+struct XSetRoot {
+    display: *mut xlib::Display,
+    root_window: xlib::Window,
+}
+
+impl XSetRoot {
+    fn new() -> Result<Self> {
         unsafe {
             let display = xlib::XOpenDisplay(ptr::null());
 
@@ -27,26 +113,15 @@ impl StatusBar {
             let screen = xlib::XDefaultScreen(display);
             let root_window = xlib::XRootWindow(display, screen);
 
-            Ok(StatusBar {
+            Ok(XSetRoot {
                 display,
                 root_window,
-                separator,
             })
         }
     }
 
-    pub fn render(
-        &self,
-        order: &[uuid::Uuid],
-        feature_map: &HashMap<uuid::Uuid, Box<feature::Feature>>,
-    ) -> Result<()> {
-        let status = order
-            .iter()
-            .map(|id| feature_map.get(id).unwrap().render())
-            .collect::<Vec<_>>()
-            .join(&self.separator);
-
-        let status_c = CString::new(status)
+    fn render(&self, text: String) -> Result<()> {
+        let status_c = CString::new(text)
             .wrap_error("render", "status text could not be converted to CString")?;
 
         unsafe {
@@ -63,7 +138,7 @@ impl StatusBar {
     }
 }
 
-impl Drop for StatusBar {
+impl Drop for XSetRoot {
     fn drop(&mut self) {
         unsafe {
             xlib::XCloseDisplay(self.display);
