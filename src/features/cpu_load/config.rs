dfe064430542e6fe0d7c7038c6694098d9a9381b
Tobias Happ <tobias.happ@gmx.de>
2018-12-22 11:37:14 +0100

    Implement ConfigEntry for cpu_load

---
+++ src/features/cpu_load/config.rs
@@ -0,0 +1,24 @@
+use super::FEATURE_NAME;
+use config::Config;
+use config::ConfigError;
+use config::Value;
+use settings::ConfigType;
+
+#[derive(Clone, Debug, Deserialize)]
+pub(crate) struct ConfigEntry {
+    pub(crate) template: String,
+    pub(crate) update_interval: u64,
+}
+
+impl ConfigType for ConfigEntry {
+    fn set_default(config: &mut Config) -> Result<(), ConfigError> {
+        config.set_default(
+            FEATURE_NAME,
+            map!(
+                "template"        => "{CL1} {CL5} {CL15}",
+                "update_interval" => 20,
+            ),
+        )?;
+        Ok(())
+    }
+}
