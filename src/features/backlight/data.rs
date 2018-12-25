4ed9475aecb0348d1c9298b40c795d1084c1dd82
Tobias Happ <tobias.happ@gmx.de>
2018-12-25 22:43:25 +0100

    Add tests for backlight data

--- src/features/backlight/data.rs
+++ src/features/backlight/data.rs
@@ -1,5 +1,5 @@
 use super::RenderConfig;
-use feature;
+use feature::Renderable;
 use utils::icon_by_percentage;
 
 #[derive(Debug)]
@@ -27,8 +27,73 @@ impl Data {
     }
 }
 
-impl feature::Renderable for Data {
+impl Renderable for Data {
     fn render(&self) -> &str {
         &self.cache
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hamcrest2::prelude::*;
+    #[cfg(feature = "mocking")]
+    use mocktopus::mocking::*;
+
+    #[test]
+    fn render_with_default() {
+        let config = RenderConfig {
+            icons: vec![],
+            template: String::from("TEMPLATE"),
+        };
+
+        let object = Data::new(config);
+
+        assert_that!(object.render(), is(equal_to("")));
+    }
+
+    #[cfg(feature = "mocking")]
+    #[test]
+    fn render_with_volume() {
+        let config = RenderConfig {
+            icons: vec![],
+            template: String::from("TEMPLATE {BL} {ICO}"),
+        };
+
+        icon_by_percentage.mock_safe(|icons, value| {
+            assert_that!(icons, empty());
+            assert_that!(value, is(equal_to(10)));
+
+            MockResult::Return(None)
+        });
+
+        let mut object = Data::new(config);
+
+        object.update(10);
+
+        assert_that!(object.render(), is(equal_to("TEMPLATE 10 {ICO}")));
+    }
+
+    #[cfg(feature = "mocking")]
+    #[test]
+    fn render_with_volume_and_icon() {
+        let config = RenderConfig {
+            icons: vec![String::from("ico1"), String::from("ico2")],
+            template: String::from("TEMPLATE {BL} {ICO}"),
+        };
+
+        icon_by_percentage.mock_safe(|icons, value| {
+            let expected_icons = vec![String::from("ico1"), String::from("ico2")];
+            assert_that!(icons, contains(expected_icons).exactly());
+            assert_that!(value, is(equal_to(10)));
+
+            MockResult::Return(Some("ICON"))
+        });
+
+        let mut object = Data::new(config);
+
+        object.update(10);
+
+        assert_that!(object.render(), is(equal_to("TEMPLATE 10 ICON")));
     }
 }
