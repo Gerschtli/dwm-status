a9697bd9ebe7924d08fd85bcc754373b41228bc9
Tobias Happ <tobias.happ@gmx.de>
2018-12-27 02:18:43 +0100

    Add test for cpu_load config

--- src/features/cpu_load/config.rs
+++ src/features/cpu_load/config.rs
@@ -21,3 +21,30 @@ impl ConfigType for ConfigEntry {
         )
     }
 }
+
+#[cfg(test)]
+#[cfg(feature = "mocking")]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+    use test_utils::config::test_set_default_err;
+    use test_utils::config::test_set_default_ok;
+
+    #[test]
+    fn config_type_set_default_when_ok() {
+        test_set_default_ok::<ConfigEntry>("cpu_load", default_map);
+    }
+
+    #[test]
+    fn config_type_set_default_when_err() {
+        test_set_default_err::<ConfigEntry>("cpu_load", default_map);
+    }
+
+    fn default_map() -> HashMap<String, Value> {
+        let mut map = HashMap::new();
+        map.insert(String::from("template"), "{CL1} {CL5} {CL15}".into());
+        map.insert(String::from("update_interval"), 20.into());
+
+        map
+    }
+}
