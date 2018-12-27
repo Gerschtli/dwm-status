d711d112b0bdc45cfc8793f4d9da0fb6dcc7a0d6
Tobias Happ <tobias.happ@gmx.de>
2018-12-27 17:12:49 +0100

    Add data for network feature

---
+++ src/features/network/data.rs
@@ -0,0 +1,100 @@
+use super::RenderConfig;
+use super::PLACEHOLDER_ESSID;
+use super::PLACEHOLDER_IPV4;
+use super::PLACEHOLDER_IPV6;
+use feature::Renderable;
+
+#[derive(Debug)]
+pub(super) struct Data {
+    cache: String,
+    config: RenderConfig,
+}
+
+impl Data {
+    pub(super) fn new(config: RenderConfig) -> Self {
+        Self {
+            cache: String::new(),
+            config,
+        }
+    }
+
+    pub(super) fn update<T4, T6, E>(&mut self, ipv4: T4, ipv6: T6, essid: E)
+    where
+        T4: Into<Option<String>>,
+        T6: Into<Option<String>>,
+        E: Into<Option<String>>,
+    {
+        self.cache = self
+            .config
+            .template
+            .replace(PLACEHOLDER_IPV4, &self.get_value(ipv4))
+            .replace(PLACEHOLDER_IPV6, &self.get_value(ipv6))
+            .replace(PLACEHOLDER_ESSID, &self.get_value(essid));
+    }
+
+    fn get_value<T: Into<Option<String>>>(&self, value: T) -> String {
+        value.into().unwrap_or(self.config.no_value.clone())
+    }
+}
+
+impl Renderable for Data {
+    fn render(&self) -> &str {
+        &self.cache
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hamcrest2::prelude::*;
+
+    #[test]
+    fn render_with_default() {
+        let object = Data::new(RenderConfig {
+            no_value: String::from("--"),
+            template: String::from("{IPv4} {IPv6} {ESSID}"),
+        });
+
+        assert_that!(object.render(), is(equal_to("")));
+    }
+
+    #[test]
+    fn render_with_update() {
+        let mut object = Data::new(RenderConfig {
+            no_value: String::from("--"),
+            template: String::from("{IPv4} {IPv6} {ESSID}"),
+        });
+
+        object.update(
+            String::from("127.0.0.1"),
+            String::from("fe::1"),
+            String::from("WLAN"),
+        );
+
+        assert_that!(object.render(), is(equal_to("127.0.0.1 fe::1 WLAN")));
+    }
+
+    #[test]
+    fn render_with_update_and_missing_placeholder() {
+        let mut object = Data::new(RenderConfig {
+            no_value: String::from("#"),
+            template: String::from("{IPv4} // {ESSID}"),
+        });
+
+        object.update(String::from("127.0.0.1"), String::from("fe::1"), None);
+
+        assert_that!(object.render(), is(equal_to("127.0.0.1 // #")));
+    }
+
+    #[test]
+    fn render_with_update_and_none_values() {
+        let mut object = Data::new(RenderConfig {
+            no_value: String::from("--"),
+            template: String::from("{IPv4} {IPv6} {ESSID}"),
+        });
+
+        object.update(None, None, None);
+
+        assert_that!(object.render(), is(equal_to("-- -- --")));
+    }
+}
--- src/features/network/mod.rs
+++ src/features/network/mod.rs
@@ -1,8 +1,10 @@
 mod config;
+mod data;
 
 pub(crate) use self::config::ConfigEntry;
 pub(self) use self::config::RenderConfig;
 pub(self) use self::config::UpdateConfig;
+pub(self) use self::data::Data;
 
 pub(super) const FEATURE_NAME: &str = "network";
 pub(self) const PLACEHOLDER_ESSID: &str = "{ESSID}";
