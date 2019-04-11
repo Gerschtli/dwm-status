880d84b213cf45e6e6a66010a01f1afc7fc618cc
Tobias Happ <tobias.happ@gmx.de>
2019-04-11 19:40:38 +0200

    Remove rust-1-31 feature

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #![deny(
     anonymous_parameters,
     bare_trait_objects,
+    clippy::all,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::perf,
+    clippy::style,
+    elided_lifetimes_in_paths,
     missing_copy_implementations,
     missing_debug_implementations,
+    single_use_lifetimes,
     trivial_casts,
     trivial_numeric_casts,
     unreachable_pub,
@@ -10,26 +19,9 @@
     unused_extern_crates,
     unused_import_braces,
     unused_qualifications,
-    variant_size_differences
+    variant_size_differences,
 )]
-#![cfg_attr(
-    feature = "rust-1-31",
-    deny(
-        clippy::all,
-        clippy::complexity,
-        clippy::correctness,
-        clippy::nursery,
-        clippy::pedantic,
-        clippy::perf,
-        clippy::style,
-        elided_lifetimes_in_paths,
-        single_use_lifetimes
-    )
-)]
-#![cfg_attr(
-    feature = "rust-1-31",
-    allow(clippy::filter_map, clippy::non_ascii_literal, deprecated)
-)]
+#![allow(clippy::filter_map, clippy::non_ascii_literal, deprecated)]
 
 #[macro_use]
 extern crate clap;
