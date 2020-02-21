1b85e34089b9401bf701e161509ce983914daa0b
Tobias Happ <tobias.happ@gmx.de>
2020-02-21 18:02:11 +0100

    Use legacyCargoFetcher

--- default.nix
+++ default.nix
@@ -21,6 +21,7 @@ rustPlatform.buildRustPackage rec {
     (path: type: type != "directory" || baseNameOf path != "target")
     ./.;
 
+  legacyCargoFetcher = true;
   cargoSha256 = "1i3zqz7yqbps684rsz422akbvhrbw6qhrmfarkm4zpi557gx400b";
 
   nativeBuildInputs = [ makeWrapper pkgconfig ];
