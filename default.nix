c1edbb87bfee8a416da66b3e6d6d0c703c01aa2b
Tobias Happ <tobias.happ@gmx.de>
2020-03-27 20:59:45 +0100

    Fix default.nix for stable channels

--- default.nix
+++ default.nix
@@ -21,7 +21,7 @@ rustPlatform.buildRustPackage rec {
     (path: type: type != "directory" || baseNameOf path != "target")
     ./.;
 
-  cargoSha256 = "0fk9fxzq4kjjr7pzfhf3j1bc2zms87hk7fcxxw49ybdkniazx9k5";
+  cargoSha256 = "1i3zqz7yqbps684rsz422akbvhrbw6qhrmfarkm4zpi557gx400b";
 
   nativeBuildInputs = [ makeWrapper pkgconfig ];
   buildInputs = [ dbus gdk_pixbuf libnotify xorg.libX11 ];
