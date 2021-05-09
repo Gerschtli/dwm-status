d565a2d0e9c36fb56ba15eb8912e48c2d87f8a51
Tobias Happ <tobias.happ@gmx.de>
2021-05-09 17:13:08 +0200

    Use pulsemixer for pulseaudio backend

--- src/features/audio/updater.rs
+++ src/features/audio/updater.rs
@@ -1,6 +1,7 @@
 use crate::error::Result;
 use crate::error::WrapErrorExt;
 use crate::feature;
+use crate::features::audio::Backend;
 use crate::wrapper::process;
 
 use super::ConfigEntry;
@@ -18,14 +19,8 @@ impl Updater {
     pub(super) const fn new(data: Data, settings: ConfigEntry) -> Self {
         Self { data, settings }
     }
-}
 
-impl feature::Updatable for Updater {
-    fn renderable(&self) -> &dyn feature::Renderable {
-        &self.data
-    }
-
-    fn update(&mut self) -> Result<()> {
+    fn update_alsa(&mut self) -> Result<()> {
         // originally taken from https://github.com/greshake/i3status-rust/blob/master/src/blocks/sound.rs
         let output = process::Command::new("amixer", &["get", &self.settings.control])
             .output()
@@ -61,5 +56,44 @@ impl feature::Updatable for Updater {
         }
 
         Ok(())
+    }
+
+    fn update_pulseaudio(&mut self) -> Result<()> {
+        let mute = process::Command::new("pulsemixer", &["--get-mute"])
+            .output()
+            .wrap_error(FEATURE_NAME, "pulsemixer --get-mute failed")?
+            .parse::<u8>()
+            .wrap_error(FEATURE_NAME, "mute status could not be read")?;
+
+        // mute is either 1 or 0
+        if mute == 1 {
+            self.data.update_mute();
+        } else {
+            let volume = process::Command::new("pulsemixer", &["--get-volume"])
+                .output()
+                .wrap_error(FEATURE_NAME, "pulsemixer --get-volume failed")?
+                .split_whitespace()
+                .next()
+                .wrap_error(FEATURE_NAME, "current volume could not be read")?
+                .parse::<u32>()
+                .wrap_error(FEATURE_NAME, "current volume could not be read")?;
+
+            self.data.update_volume(volume);
+        }
+
+        Ok(())
+    }
+}
+
+impl feature::Updatable for Updater {
+    fn renderable(&self) -> &dyn feature::Renderable {
+        &self.data
+    }
+
+    fn update(&mut self) -> Result<()> {
+        match self.settings.backend {
+            Backend::Alsa => self.update_alsa(),
+            Backend::Pulseaudio => self.update_pulseaudio(),
+        }
     }
 }
